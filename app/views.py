0fadf5ccfd40437222ee1941166a3159cad8774c
Your Name <you@example.com>
2024-01-03 19:50:52 +0100

    adding views

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,request
-# from models import *
+from . import models
 def home (request):
-    
+    pays = models.Pays.objects.create(nom_pays = "Maroc")
+    ville = models.Ville.objects.create(nom_ville ="Agadir",id_pays = pays.id_pays)
+    hotel = models.Hotel.objects.create(prix_nuit=250,type_chambre="Deluxe",n_chambreDispo=3,nom_hotel="AL Sahara",id_ville=ville.id_ville)
+    voyage = models.Voyage.objects.create(prix_voyage=1500,duree_voyage=7,transport=True,id_hotel=hotel.id_hotel,)
     return render(request, 'home.html', {})
 
 
-# if __name__ == "__main__":
-#     pays = Pays.objects.create("Maroc")
-#     ville = Ville.objects.create("Agadir",pays.id_pays)
-#     voyage = Voyage.objects.create(1500,7,)
+    
